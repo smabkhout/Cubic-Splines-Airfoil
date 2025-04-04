8068a423b30a595d7c71e8a430e2f516da3f73bc
Anas Benbaki <abenbaki@enseirb-matmeca.fr>
2025-04-04 12:05:03 +0200

    version initiale: integration left & right & trapeze

--- src/integration.py
+++ src/integration.py
@@ -1,28 +1,26 @@
 # Question 1
 # Intégrale: left and right rectangle,trapezoid
 
+def integration_n_left_rec( f, a, b, N ):
+    I=0
+    h = (b-a)/N
+    for i in range(N):
+        I+= f(a + i*h)
+    return h * I
 
+def integration_n_right_rec( f, a, b, N ):
+    I=0
+    h = (b-a)/N
+    for i in range(1, N+1):
+        I+= f(a + i*h)
+    return h * I
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def integration_n_trapezoid( f, a, b, N ):
+    I=0
+    h = (b-a)/N
+    for i in range(1, N):
+        I+= f(a + i*h)   
+    I += (f(a) + f(b))/2 
+    return h * I
 
 #Intégrale: middle point, Simpson

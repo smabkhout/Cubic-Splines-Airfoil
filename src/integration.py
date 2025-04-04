b8c7cb52c15217938aed55b00c54e87ff754a4db
EL ABBOUDI Manal <Manal.El_Abboudi@bordeaux-inp.fr>
2025-04-04 12:10:19 +0200

    simpson and middle point method

--- src/integration.py
+++ src/integration.py
@@ -24,3 +24,26 @@ def integration_n_trapezoid( f, a, b, N ):
     return h * I
 
 #Intégrale: middle point, Simpson
+def integration_n_middle_point(f,a,b,n):
+    #calcul de largeur des sous intervalles
+    h=(b-a)/n
+    #construction d'une liste des images  des noeuds de quadrature
+    l=[f(a+(2*k+1)*h/2) for k in range(n)]
+    s=0
+    for i in range(n):
+       s+=l[i]
+    return s*h   
+
+def integration_n_simpson(f,a,b,n):
+     #calcul de largeur des sous intervalles
+    h=(b-a)/(n+1)
+    #construction d'une liste  des noeuds de quadrature
+    l=[a+k*h for k in range(1,n+1)]
+    s=0
+    d=0
+    for i in range(n+1):
+       s+=f(l[i]+h/2)
+    for i in range(1,n+1):
+       d+=f(l[i])
+    return (h/6)*(f(a)+f(b))+(2*h/3)*s+(h/3)*d
+

9195051c9701231f1ed56dcd6cec9b4546fb809f
MABKHOUT Salah-Eddine <Salah-Eddine.Mabkhout@bordeaux-inp.fr>
2025-04-11 10:18:28 +0200

    modifs

--- src/airfoil.py
+++ src/airfoil.py
@@ -34,36 +34,22 @@ def cubic_spline(x, y):
     coefficients = [(y[i], b[i], c[i], d[i]) for i in range(n)]
     return coefficients
 
-# Fonction pour évaluer la spline à un point donné
+# Fonction pour évaluer la spline à un point donné # On va evaluer les ordonnees des points de x_vals
 def evaluate_spline(x_vals, x, coefficients):
     n = len(x) - 1
     y_vals = []
-
     for x_val in x_vals:
         # Trouver le bon intervalle
         for i in range(n):
-            if x[i] <= x_val <= x[i + 1]:
+            if x[i] <= x_val <= x[i + 1]: # la liste x contient les intervalles par selon lesquels la spline est definie
                 a, b, c, d = coefficients[i]
                 dx = x_val - x[i]
+                count += 1
                 y_vals.append(a + b * dx + c * dx**2 + d * dx**3)
                 break
-
     return np.array(y_vals)
 
-# Exemple d'utilisation
-x = np.array([7.70000e-05,4.71000e-04,1.52700e-03,2.84400e-03,4.34000e-03,6.02500e-03
-,7.92300e-03,1.00400e-02,1.23870e-02,1.49810e-02,1.78250e-02,2.09310e-02
-,2.43130e-02,2.79870e-02,3.19660e-02,3.62650e-02,4.09020e-02,4.58960e-02
-,5.12600e-02,5.70090e-02,6.31590e-02,6.97260e-02,7.67270e-02,8.41770e-02
-,9.20920e-02,1.00487e-01,1.09377e-01,1.18778e-01,1.28704e-01,1.39169e-01
-,1.50184e-01,1.61763e-01,1.73919e-01,1.86664e-01,2.00007e-01,2.13958e-01
-,2.28525e-01,2.43715e-01,2.59532e-01,2.75981e-01,2.93062e-01,3.10776e-01
-,3.29120e-01,3.48087e-01,3.67669e-01,3.87856e-01,4.08632e-01,4.29979e-01
-,4.51876e-01,4.74296e-01,4.97207e-01,5.20573e-01,5.44351e-01,5.68493e-01
-,5.92942e-01,6.17641e-01,6.42522e-01,6.67505e-01,6.92501e-01,7.42170e-01
-,7.90661e-01,8.58986e-01,8.99952e-01,9.35728e-01,9.64902e-01,9.85886e-01
-,1.00000e+00])
-print(x.shape[0])
+x = np.array([1, 2, 2.5, 3.5, 5])
 y = np.array([2, 3, 5, 4, 6])
 
 # Calcul des coefficients de la spline cubique
